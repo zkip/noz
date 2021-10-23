4abfa0e062950f7aa277db9b34c95e85603af101
zkip <zkiplan@qq.com>
2021-10-23 13:58:22 +0800

    add array tools lastOne.

--- packages/terminal-browser/src/utils/array.ts
+++ packages/terminal-browser/src/utils/array.ts
@@ -1,8 +1,10 @@
 export const inBound = (a: number, b: number) => (v: number) =>
-	Math.max(Math.min(a, b), Math.min(v, Math.max(a, b)));
+    Math.max(Math.min(a, b), Math.min(v, Math.max(a, b)));
 
 export const inArrayBound = <T>({ length }: ArrayLike<T>) =>
-	inBound(0, length - 1);
+    inBound(0, length - 1);
 
 export const isInRange = (min: number, max: number) => (n: number) =>
-	n >= min && n < max;
+    n >= min && n < max;
+
+export const lastOne = <T>(list: Array<T>) => list[list.length - 1];
